413f7e1dfb35840c8be4d1157a0cf10bb467f634
aadn_Abhishek <75962986+Aadn-abhishek@users.noreply.github.com>
2024-05-14 11:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.querySelector('form');
+
+//     form.addEventListener('submit', function(event) {
+//         event.preventDefault();
+
+//         const name = form.querySelector('input[type="text"]').value;
+//         const email = form.querySelector('input[type="email"]').value;
+//         const phone = form.querySelector('input[type="tel"]').value;
+//         const creditCard = form.querySelector('input[type="text"]').value;
+
+//         // You can perform further validation or processing of the form data here
+//         console.log('Form submitted with the following data:');
+//         console.log('Name:', name);
+//         console.log('Email:', email);
+//         console.log('Phone:', phone);
+//         console.log('Credit Card:', creditCard);
+
+//         // You can also add code here to send the form data to a server using AJAX
+//     });
+// });
